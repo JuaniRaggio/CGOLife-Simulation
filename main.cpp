e90ed3b38f053d9a87a0589c426345a9f08cf952
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-01-10 00:20:39 -0300

    Error check macro

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
+#include <exception>
 #include <iostream>
 #include <SDL2/SDL.h>
 
+#define CHECK_ERRORS(expresion)\
+        do {\
+            if ((expresion)) {\
+                std::cout << "Assertion failed at line " << __LINE__ << " at file " << __FILE__ << std::endl;\
+                std::terminate();\
+            }\
+        } while(0)
+
 int main(void) {
     std::cout << "Hello world!" << std::endl;
-    return 0;
+    CHECK_ERRORS(true);
+    std::cout << "Hey!" << std::endl;
 }
 
