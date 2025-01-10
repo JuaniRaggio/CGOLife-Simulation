eb4980302848cb918ced604993f958c359effc01
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-01-10 00:31:07 -0300

    Trying workspace, start implementing tomorrow

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <iostream>
 
+class Simulation {
+    private:
+        Simulation();
+        void prueba(void);
+    public:
+        int attribute;
+};
 
---
+++ src/simulation.cpp
@@ -0,0 +1,8 @@
+#include "../include/simulation.hpp"
+
+Simulation::Simulation() : attribute(0) {};
+
+void Simulation::prueba() {
+    std::cout << "Tryout!\n";
+}
+
